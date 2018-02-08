542306922f33f13fbb3379dc7a0e5f6fc56abb21
Paul Pinterits <rawing7@gmail.com>
2018-02-08 02:40:14 +0100

    added documentation for "misc"-category functions

--- introspection/misc.py
+++ introspection/misc.py
@@ -8,6 +8,13 @@ __all__ = ['common_ancestor']
 
 
 def common_ancestor(*cls_list):
+    """
+    Finds the closest common parent class of the given classes.
+
+    :param cls_list: a list of classes
+    :return: the given classes' shared parent class
+    """
+
     mros = [deque(inspect.getmro(cls)) for cls in cls_list]
     track = defaultdict(int)
     while mros:
