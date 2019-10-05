9d1df38e9fa93ad8a933c2910ab60d6a08faa43e
Aran-Fey <rawing7@gmail.com>
2019-10-05 22:07:57 +0200

    fixed module import again

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ author = "Aran-Fey"
 name = HERE.name
 long_description = (HERE / "README.md").read_text()
 
-module = import_package_from_path(name)
+module = import_package_from_path(HERE / name)
 
 setuptools.setup(
     name=name,
